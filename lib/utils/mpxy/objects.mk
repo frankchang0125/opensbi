27acac56202ae783059e8c5b99d82e29d34f03b1
Alvin Chang <alvinga@andestech.com>
2025-02-23 23:50:01 +0800

    lib: utils: Add MPXY RPMI Request Forward driver
    Implement MPXY RPMI Request Forward driver which stores RPMI message
    forwarded by other RPMI clients, e.g., untrusted domain.
    
    Signed-off-by: Alvin Chang <alvinga@andestech.com>
    Co-developed-by: Yu-Chien Peter Lin <peter.lin@sifive.com>
    Signed-off-by: Yu-Chien Peter Lin <peter.lin@sifive.com>

--- lib/utils/mpxy/objects.mk
+++ lib/utils/mpxy/objects.mk
@@ -26,3 +26,6 @@ libsbiutils-objs-$(CONFIG_FDT_MPXY_RPMI_VOLTAGE) += mpxy/fdt_mpxy_rpmi_voltage.o
 
 carray-fdt_mpxy_drivers-$(CONFIG_FDT_MPXY_RPMI_DEVICE_POWER) += fdt_mpxy_rpmi_device_power
 libsbiutils-objs-$(CONFIG_FDT_MPXY_RPMI_DEVICE_POWER) += mpxy/fdt_mpxy_rpmi_device_power.o
+
+carray-fdt_mpxy_drivers-$(CONFIG_FDT_MPXY_REQFWD) += fdt_mpxy_reqfwd
+libsbiutils-objs-$(CONFIG_FDT_MPXY_REQFWD) += mpxy/fdt_mpxy_reqfwd.o
